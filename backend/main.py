9b943b4675e88718e79786bfe09d67854c70e668
Balazs Laczko <laczbali@gmail.com>
2022-01-22 16:02:20 +0100

    feat: added code to test DirtRally2 handler

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,13 @@
 from classes.base.AppSettings import AppSettings
 from classes.base.UdpHandler import UdpHandler
+from classes.game.GameDirtRally2 import GameDirtRally2
 
 def main():
-    udp = UdpHandler()
-    udp.start_listen(20777)
 
-    while True:
-        in1 = input("Command: ")
 
-        if in1 == "exit":
-            break
-        elif in1 == "read":
-            print(udp.get_data())
-        elif in1 == "start":
-            udp.start_listen(20777)
-        elif in1 == "stop":
-            udp.stop_listen()
+    gh = GameDirtRally2()
+    gh.start_listening()
+    gh.parse_udp_data()
 
 
 if __name__ == "__main__":
