ae4129e7f33ca330233e9056fb74b8d48f24d416
Balazs Laczko <laczbali@gmail.com>
2022-01-29 12:02:52 +0100

    feat: added run start/stop to webapi

--- backend/classes/game/GameWrapper.py
+++ backend/classes/game/GameWrapper.py
@@ -1,8 +1,11 @@
+from classes.game.RunData import RunData
 from classes.game.GameHandler import GameHandler
 from classes.game.GameDirtRally2 import GameDirtRally2
 
 
 class GameWrapper:
+
+    game_instance: GameHandler = None
 
     def get_game_list():
         """
@@ -40,3 +43,60 @@ class GameWrapper:
 
         # return the attributes of the class
         return game_class.get_attributes()
+
+
+
+    def start_run(parameters):
+        """
+        Starts a new run of a game
+        """
+        try:
+            # shut down previous instance
+            if GameWrapper.game_instance is not None:
+                GameWrapper.game_instance.shutdown()
+
+            # get game_name
+            game_name = parameters["game_name"]
+
+            #  get all classes that inherit from GameHandler (only works for classes that are imported here)
+            game_classes = GameHandler.__subclasses__()
+
+            # get the class that matches the game name
+            game_class: GameHandler = list(
+                filter(
+                    lambda x: x.__name__.replace("Game", "") == game_name,
+                    game_classes
+                )
+            )[0]
+
+            # set up RunData (if keys other than the game_name are present)
+            if len(list(parameters.keys())) > 1:
+                run_data = RunData()
+                run_data.set_parameters(parameters)
+            else:
+                run_data = None
+
+            # start the run
+            GameWrapper.game_instance: GameHandler = game_class()
+            GameWrapper.game_instance.start_run(run_data)
+
+            return "ok"
+
+        except Exception as e:
+            return "Could not start run" + "\n" + str(e)
+
+
+
+    def stop_run():
+        """
+        Stops the current run of a game
+        """
+        try:
+            # shut down previous instance
+            if GameWrapper.game_instance is not None:
+                GameWrapper.game_instance.shutdown()
+
+            return "ok"
+
+        except Exception as e:
+            return "Could not stop run" + "\n" + str(e)
--- backend/classes/webapi/FlaskApp.py
+++ backend/classes/webapi/FlaskApp.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+import logging
 
 from classes.webapi.JsonResponse import JsonResponse
 from classes.game.GameWrapper import GameWrapper
@@ -12,6 +13,8 @@ class FlaskApp:
             origins=["http://127.0.0.1"],
             supports_credentials=True
     )
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
     
 
 
@@ -44,3 +47,28 @@ class FlaskApp:
 
         # return attributes of game
         return JsonResponse.make_response(GameWrapper.get_game_attributes(game_name))
+
+
+
+    @app.route("/game/start", methods=["POST"])
+    def start_run():
+        """
+        Starts a new run of a game
+        """
+
+        # get parameters from request
+        parameters = request.get_json()
+
+        # start the run
+        return JsonResponse.make_response(GameWrapper.start_run(parameters))
+
+
+
+    @app.route("/game/stop", methods=["POST"])
+    def stop_run():
+        """
+        Stops the current run of a game
+        """
+
+        # stop the run
+        return JsonResponse.make_response(GameWrapper.stop_run())
