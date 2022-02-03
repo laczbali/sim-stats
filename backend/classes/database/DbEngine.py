1250964c9395d1e4ada9fc7f1ecc9af5bf23e8db
Balazs Laczko <laczbali@gmail.com>
2022-02-03 20:39:26 +0100

    feat: added default values for db

--- backend/classes/database/DbEngine.py
+++ backend/classes/database/DbEngine.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from classes.database.models.Base import Base
+from classes.database.models.Game import Game
 
 
 # db file is at ./backend/db/db.sqlite3
@@ -15,3 +17,12 @@ engine = create_engine(
 
 # create table if missing
 Base.metadata.create_all(engine)
+
+# add default contents
+with Session(engine) as session:
+    # add "DirtRally2" game if missing
+    game = session.query(Game).filter_by(name="DirtRally2").first()
+    if game is None:
+        game = Game(name="DirtRally2")
+        session.add(game)
+        session.commit()
